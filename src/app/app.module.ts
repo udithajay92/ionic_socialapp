b0a72176ad5683cedfd782ff065684f1e7190a33
Uditha Jay <Uditha Jay>
2017-03-18 14:12:34 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { AuthService } from '../providers/auth-service';
+import { RegisterPage } from '../pages/register/register';
+ 
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    RegisterPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    RegisterPage
+  ],
+  providers: [AuthService]
+})
+export class AppModule {}
